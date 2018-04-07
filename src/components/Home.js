718ef783780c8dde852b1da140a816dde1228e9f
Eric Dudley <ericdudley@email.com>
2018-04-07 14:33:53 -0400

    Starting to work on home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,74 @@ import '../styles/home.scss';
 import { Link } from 'react-router';
 class Home extends React.Component {
     render() {
+        let apps = [
+            {
+                name: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/eric-dudley-894721106',
+                icon: 'linkedin',
+                same_page: false
+            },
+            {
+                name: 'GitLab',
+                url: 'https://gitlab.com/ericdudley/personal-website',
+                icon: 'gitlab',
+                same_page: false
+            },
+            {
+                name: 'Email',
+                url: 'mailto:ericdudley@email.com',
+                icon: 'envelope',
+                same_page: false
+            },
+            {
+                name: 'GitHub',
+                url: 'https://github.com/ericdudley',
+                icon: 'github',
+                same_page: false
+            },
+            {
+                image: '../images/me.jpg'
+            },
+            {
+                name: 'Flickr',
+                url: 'https://www.flickr.com/photos/ericdudley',
+                icon: 'flickr',
+                same_page: false
+            },
+            {
+                name: 'Stretch',
+                url:
+                    '/stretch',
+                icon: 'man',
+                same_page: true
+            }
+        ];
+
+        let apps_markup = [];
+        for (let app of apps) {
+            apps_markup.push(
+                <a href={app.url} target={app.same_page ? "_self" : "_blank"}>
+                    <div className="app-wrapper">
+                        {app.image &&
+                            <div className="app-image"></div>
+                        }{app.icon &&
+                            <div><i className={"app-icon fa fa-" + app.icon}></i>
+                                <span className="app-name">{app.name}</span></div>
+                        }
+                    </div>
+                </a>
+            );
+        }
+
+
+
+
         return (
             <div id="home">
                 <section>
+                    {apps_markup}
+                </section>
+                {/* <section>
                 <div id="home-links">
                 <a href="https://github.com/ericdudley" target="_blank" className="link link1">programmer</a>          
                 <Link to={`/photographer`} className="link link2">photographer</Link>
@@ -22,7 +87,7 @@ class Home extends React.Component {
                         <li className="home-icon"><a href="https://www.linkedin.com/in/eric-dudley-894721106" target="_blank"><i className="fa fa-5x fa-linkedin" /></a></li>
                         <li className="home-icon"><a href="mailto:ericdudley@email.com" target="_blank"><i className="fa fa-5x fa-envelope" /></a></li>
                     </ul>
-                </section>
+                </section> */}
             </div>
         );
     }
